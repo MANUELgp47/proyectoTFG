e9f10307664dc30f8c5b8ec1af035e17de92f802
Manuel <manuelgp4747@gmail.com>
2026-06-08 10:00:45 +0200

    Configurado dominio www.memora-tfg.es

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -11,7 +11,7 @@ import axios from 'axios';
 const api = axios.create({
     //baseURL: 'https://proyectotfg-production-22de.up.railway.app/api',
     //baseURL: 'http://localhost:3000/api',
-    baseURL: 'http://www.memora-tfg:3000/api',
+    baseURL: 'https://www.memora-tfg.es:3000/api',
 });
 
 // Interceptor: añade el token a cada request
